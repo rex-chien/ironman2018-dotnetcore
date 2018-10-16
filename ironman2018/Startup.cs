9927d1a65d6b17fc31f4cd07c8392a5030d008eb
Rex.Chien <lovestar5317@gmail.com>
2018-10-16 22:58:54 +0800

    用 inlined middleware 來設定網站語系

--- ironman2018/Startup.cs
+++ ironman2018/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using ironman2018.Models.DependecyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -14,6 +17,8 @@ namespace ironman2018
 {
     public class Startup
     {
+        private static readonly string[] EnabledCultures = new[] { "zh-tw", "en-us", "de-de" };
+
         private readonly IConfiguration _config;
         private readonly IHostingEnvironment _env;
 
@@ -32,6 +37,28 @@ namespace ironman2018
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.Use((context, next) =>
+            {
+                var path = context.Request.Path;
+
+                var match = Regex.Match(path, $"/({string.Join('|', EnabledCultures)})");
+                var defaultCulture = "zh-tw";
+                if (match.Success)
+                {
+                    defaultCulture = match.Groups[1].Value;
+                }
+
+                var culture = new CultureInfo(defaultCulture);
+
+                CultureInfo.CurrentCulture = culture;
+                CultureInfo.CurrentUICulture = culture;
+
+                // Call the next delegate/middleware in the pipeline
+                return next();
+            });
+
+            app.UseStaticFiles();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
