7c36bf03a1c4e2b2e366b553019e1ef61fe01da8
Rex.Chien <lovestar5317@gmail.com>
2018-10-20 23:07:50 +0800

    no message

---
+++ ironman2018/StartupDevelopment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ironman2018.Middlewares;
+using ironman2018.Models.DependecyInjection;
+using ironman2018.Models.Environments;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+namespace ironman2018
+{
+    public class StartupDevelopment
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IEnvironmentsService, DevelopmentEnvironmentsService>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Enviroments}/{action=StartupClassConvention}/{id?}");
+            });
+        }
+    }
+}
